7e4558a46bdd93b4b3a38e73ab51affd4e02a2f1
danielguitara <guitara.daniel@gmail.com>
2015-09-16 17:08:06 +0700

    Update
    Fix zeppelin and preloader

--- js/app.js
+++ js/app.js
@@ -581,14 +581,15 @@ function initImagesLoaded(){
 		valProgress++   ;
 		setProgress(valProgress,imgLen);
 	}).done(function(e){
-		$('.dcc-preloader').delay(2000).fadeOut(600);
+		$('.dcc-preloader').delay(1500).fadeOut(300);
 		checkImagesLoaded();
+        $('.predcc-zeppelin').addClass("dcc-zeppelin");
 	});
 }
 function setProgress(valProgress,imgLen){
 	var val = (valProgress / imgLen) * 100;
 	$('.progress').css('height',val+'%');
-	$('.progress-val').html(val+'%');
+	$('.progress-val').html(val.toFixed(0)+'%');
 } 
 
 function loadHQ(){
