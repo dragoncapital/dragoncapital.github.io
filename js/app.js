8b403388404e52981fc2bc3204943c7a2e80a3c1
Ervandra Halim <ervandra.halim@gmail.com>
2015-09-21 13:09:37 +0700

    van

--- js/app.js
+++ js/app.js
@@ -101,8 +101,6 @@ jQdcc(document).ready(function($){
 		$('.rocket-smoke').css({'margin-left':'-'+((nWx*0.3)/2)+'px'});
 		$('.rocket-fire').css({'margin-left':'-'+((nWx*0.025)/2)+'px'})
 		$('.btn-launch').css({'margin-left':'-'+($('.btn-launch').width()/2)+'px'});
-		//$('.btn-launch:before').css({'width':(rocket*0.51)+'px', 'height':(rocket*0.51)+'px', 'margin-left':'-'+((rocket*0.51)/2)+'px'});
-
 		$('.reveal-modal .hiring-content, .team-reveal-modal .details, .menu-items, .popup-content').perfectScrollbar('update');		
 	}
 
